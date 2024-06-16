e8bd78f500a60c5f1f6d84c8f5bed4168948e807
Mohammad Javad Bakhshi <mjavad.bakhshi@gmail.com>
2024-06-16 16:22:38 +0330

    Change handling multi touch of Clickable component by pure js event

--- src/compnents/ClickableButton.jsx
+++ src/compnents/ClickableButton.jsx
@@ -1,33 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
-import Hammer from "hammerjs";
+import React, { useState } from "react";
 
 function ClickableButton() {
   const [touches, setTouches] = useState(0);
 
-  const buttonRef = useRef(null);
-  const hammerRef = useRef(null);
-
-  useEffect(() => {
-    const button = buttonRef.current;
-    hammerRef.current = new Hammer(button);
-
-    hammerRef.current.on("tap", (event) => {
-      setTouches(touches + event.tapCount);
-    });
-
-    // hammerRef.current.on("press", (event) => {
-    //   console.log("Press event detected!");
-    // });
-
-    // hammerRef.current.on("release", (event) => {
-    //   console.log("Release event detected!");
-    // });
-  }, []);
+  const handleButtonTouched = (event) => {
+    setTouches(event.changedTouches.length + touches);
+  };
 
   return (
     <>
       <h1 className="text-white">{touches}</h1>
-      <div ref={buttonRef} id="logo" className="circle-button ">
+      <div
+        onTouchEnd={handleButtonTouched}
+        id="logo"
+        className="circle-button "
+      >
         <img src={require("../images/logo.png")} />
       </div>
     </>
